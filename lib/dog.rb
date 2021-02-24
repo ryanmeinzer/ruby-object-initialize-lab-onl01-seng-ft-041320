483cd262fbbda2af9688f9025103ee3d3c940e38
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-24 06:33:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,20 +5,4 @@ class Dog
     @breed = breed
   end
   
-  # def name=(name)
-  #   @name = name
-  # end
-  
-  # def breed=(breed = "Mutt")
-  #   @breed = breed
-  # end
-  
-  # def name
-  #   @name
-  # end
-  
-  # def breed 
-  #   @breed
-  # end
-  
 end
