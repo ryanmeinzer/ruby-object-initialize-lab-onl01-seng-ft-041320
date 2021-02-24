7d56d8766a992808dab912825f7f5ae5cde31c74
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-02-24 06:31:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,20 @@
-class Person 
+class Dog
   
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
   end
   
-  def name=(name)
+  def name=(name, breed = "Mutt")
     @name = name
+    @breed = breed
   end
   
   def name
     @name
+  end
+  
+  def breed 
+    @breed
   end
   
 end
